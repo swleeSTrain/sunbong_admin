84b817a18c30482e72cf992572fdcefdcadc5242
Sangwon Lee <sang4939@gmail.com>
2024-10-21 11:20:23 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex justify-center space-x-4">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo transition hover:drop-shadow-[0_0_2em_#646cffaa]" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo transition hover:drop-shadow-[0_0_2em_#42b883aa]" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+/* Tailwind로 교체했으므로 기존 스타일을 제거합니다 */
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 className="text-sm font-semibold text-gray-300 underline">테일윈드 테스트</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
