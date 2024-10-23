c1a6f2dae156d6cea289e86949a6609a009edf78
Daejin Kim <sp04212016@gmail.com>
2024-10-23 11:26:09 +0900

    feature/ 리스트, 등록 기능 초안

--- src/routers/notice.js
+++ src/routers/notice.js
@@ -11,7 +11,7 @@ const noticeRouting ={
     component: IndexPage,
     children: [
         { path: 'list', component: NoticeListPage },
-        { path: '', redirect: '/notice/list?page=1' },  // 기본 경로 리디렉션
+
         { path: 'read/:noticeNo', component: NoticeReadPage },
         { path: 'edit/:noticeNo', component: NoticeEditPage },
         { path: 'add', component: NoticeAddPage }
